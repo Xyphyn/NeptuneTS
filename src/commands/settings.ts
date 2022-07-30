fe081fe7e8dcd5dcfeec86b3f6b07c9ff4992123
Xyphyn <xyphyn@outlook.com>
2022-07-30 15:00:40 -0700

    Fixed startup error

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -26,7 +26,7 @@ export const data = new SlashCommandBuilder()
             )
             .addSubcommand((subcommand) =>
                 subcommand
-                    .setName('warningThreshold')
+                    .setName('warningthreshold')
                     .setDescription(
                         'The amount of warnings before a user is muted.'
                     )
