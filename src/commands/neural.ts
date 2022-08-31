7b9dfdb77baee5d4957dc85800bf779787f69422
Xyphyn <xyphyn@outlook.com>
2022-08-31 09:12:34 -0700

    Fixed grammar

--- src/commands/neural.ts
+++ src/commands/neural.ts
@@ -29,15 +29,30 @@ export const data: Command = {
                     required: true
                 }
             ]
+        },
+        {
+            type: ApplicationCommandOptionType.Subcommand,
+            name: 'catdog',
+            description: 'Model that can distinguish between cats and dogs.',
+            options: [
+                {
+                    type: ApplicationCommandOptionType.Attachment,
+                    name: 'image',
+                    description: 'The pet for the network to recognize.',
+                    required: true
+                }
+            ]
         }
     ]
 }
 
 export const execute = async (interaction: ChatInputCommandInteraction) => {
     await interaction.deferReply()
+
     const attachment = interaction.options.getAttachment('image')!
     const allowedTypes = ['png', 'jpg', 'jpeg', 'bmp']
     const attachmentType = attachment.url.split('.').slice(-1)[0]
+    const subcommand = interaction.options.getSubcommand()
 
     if (!allowedTypes.includes(attachmentType)) {
         await interaction.editReply({
@@ -47,41 +62,51 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
         return
     }
 
-    let res: any
-    try {
-        res = await fetch('http://local.xylight.us:80/neural/digit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                imgUrl: `${attachment.url}`,
-                type: attachmentType
-            })
-        }).catch(async (e) => {
-        	await interaction.editReply({
-        		embeds: [error("Neptune's neural network servers are currently offline.")]
-        	})
-        	return
-        })
-    } catch {
-        await interaction.editReply({
-            embeds: [error("Neptune's neural network servers are offline.")]
-        })
-        return
+    const urls: any = {
+        digit: '/neural/digit',
+        catdog: '/neural/catdog'
     }
 
-    const json = await res!.json()
-
-    const embed = new EmbedBuilder()
-        .setImage(`${attachment.url}`)
-        .setDescription(`The network thinks this is a(n) ${json.guess}`)
-        .setColor(globalConfig.embedColor)
-        .setFooter({
-            text: 'The network does best on centered, 1:1, grayscale images.'
+    const res = await fetch(`http://local.xylight.us:80${urls[subcommand]}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            imgUrl: `${attachment.url}`,
+            type: attachmentType
         })
-
-    await interaction.editReply({
-        embeds: [embed]
+    }).catch(async (e) => {
+        await interaction.editReply({
+            embeds: [
+                error("Neptune's neural network servers are currently offline.")
+            ]
+        })
+        return
     })
+
+    const json: any = await res!.json()
+
+    switch (subcommand) {
+        case 'digit': {
+            const embed = new EmbedBuilder()
+                .setImage(`${attachment.url}`)
+                .setDescription(
+                    `The network thinks this is a${
+                        json.guess == '8' ? 'n' : ''
+                    } ${json.guess}`
+                )
+                .setColor(globalConfig.embedColor)
+                .setFooter({
+                    text: 'This model does best on images with black/transparent backgrounds.'
+                })
+
+            await interaction.editReply({
+                embeds: [embed]
+            })
+            break
+        }
+        case 'catdog': {
+        }
+    }
 }
