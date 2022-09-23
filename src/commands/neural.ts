d8a828331911c00914ae458d9fcc7bfde923bdd8
Xyphyn <xyphyn@outlook.com>
2022-09-23 14:30:18 -0700

    Added contribution features

--- src/commands/neural.ts
+++ src/commands/neural.ts
@@ -1,9 +1,15 @@
 import {
+    ActionRowBuilder,
     ApplicationCommandOptionType,
     Attachment,
     AttachmentBuilder,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
     ChatInputCommandInteraction,
-    EmbedBuilder
+    EmbedBuilder,
+    Interaction,
+    MessageComponentInteraction
 } from 'discord.js'
 import { globalConfig } from '../config/globalConfig.js'
 import { error } from '../managers/errorManager.js'
@@ -11,6 +17,7 @@ import { Command } from '../types/types'
 import { loading } from '../util/tools.js'
 import fs from 'fs'
 import fetch from 'node-fetch'
+import { v4 as uuid } from 'uuid'
 
 export const data: Command = {
     name: 'neural',
@@ -67,6 +74,16 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
         catdog: '/neural/catdog'
     }
 
+    const downloadFile = async (url: string, path: string) => {
+        const res = await fetch(url)
+        const fileStream = fs.createWriteStream(path)
+        await new Promise((resolve, reject) => {
+            res.body!.pipe(fileStream)
+            res.body!.on('error', reject)
+            fileStream.on('finish', resolve)
+        })
+    }
+
     const res = await fetch(
         `http://${process.env.NEURAL_SERVER}${urls[subcommand]}`,
         {
@@ -110,6 +127,17 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
             break
         }
         case 'catdog': {
+            const rows: any = new ActionRowBuilder().addComponents(
+                new ButtonBuilder()
+                    .setCustomId('np-neural-incorrect')
+                    .setStyle(ButtonStyle.Danger)
+                    .setLabel('Incorrect'),
+                new ButtonBuilder()
+                    .setCustomId('np-neural-correct')
+                    .setStyle(ButtonStyle.Success)
+                    .setLabel('Correct')
+            )
+
             const embed = new EmbedBuilder()
                 .setImage(`${attachment.url}`)
                 .setDescription(`The model thinks this is a ${json.guess}`)
@@ -118,9 +146,69 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
                     text: 'This model does best on centered, 1:1 images of cats and dogs.'
                 })
 
+            const collector =
+                interaction.channel!.createMessageComponentCollector({
+                    idle: 30000,
+                    filter: (i: Interaction) => {
+                        return i.user == interaction.user
+                    }
+                })
+
+            collector.on(
+                'collect',
+                async (cmpInteraction: MessageComponentInteraction) => {
+                    if (!cmpInteraction.isButton()) return
+                    if (!fs.existsSync('data')) {
+                        fs.mkdirSync('data')
+                    }
+                    if (!fs.existsSync('data/cat')) {
+                        fs.mkdirSync('data/cat')
+                    }
+                    if (!fs.existsSync('data/dog')) {
+                        fs.mkdirSync('data/dog')
+                    }
+                    if (cmpInteraction.customId == 'np-neural-incorrect') {
+                        let dir = `data/${json.guess}`
+                        if (json.guess == 'cat') dir = 'data/dog'
+                        else dir = 'data/cat'
+
+                        downloadFile(
+                            attachment.url,
+                            `${dir}/${uuid()}.${attachmentType}`
+                        )
+                    }
+                    if (cmpInteraction.customId == 'np-neural-correct') {
+                        let dir = `data/${json.guess}`
+                        if (json.guess == 'cat') dir = 'data/cat'
+                        else dir = 'data/dog'
+
+                        downloadFile(
+                            attachment.url,
+                            `${dir}/${uuid()}.${attachmentType}`
+                        )
+                    }
+
+                    await cmpInteraction.reply({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle('Submitted')
+                                .setDescription(
+                                    "Thanks for helping Neptune's neural network get better! Note that abuse can get you permanently banned from using Neptune."
+                                )
+                                .setColor(globalConfig.embedColor)
+                        ],
+                        ephemeral: true
+                    })
+
+                    collector.stop()
+                }
+            )
+
             await interaction.editReply({
-                embeds: [embed]
+                embeds: [embed],
+                components: [rows]
             })
+
             break
         }
     }
